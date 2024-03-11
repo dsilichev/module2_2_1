5142ca409747f05a62c8d055a5a5700498e07a22
Dmitrii Ilichev <dsilichev@gmail.com>
2024-03-11 07:11:03 +0300

    functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import { useState } from 'react';
 
 function App() {
+  let [isValueVaild, setIsValueVaild] = useState(false);
+  let [value, setValue] = useState(null);
+  let [list, setList] = useState([]);
+  let [error, setError] = useState(null);
+
+  const onInputButtonClick = () => {
+    const promptValue = prompt('Введите значение');
+    if (promptValue.length < 3) {
+      setIsValueVaild(false);
+      setError('Введенное значение должно содержать минимум 3 символа');
+    } else {
+      setIsValueVaild(true);
+      setError(null);
+      setValue(promptValue);
+    }
+  }
+
+  const onAddButtonClick = () => {
+    if (isValueVaild) {
+      const id = Date.now();
+      const updatedList = [...list, { id, value }]
+      setList(updatedList);
+      setError(null);
+      setValue('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1 className={styles["page-heading"]}>Ввод значения</h1>
+      <p className={styles["no-margin-text"]}>
+        Текущее значение <code>value</code>: "<output className={styles["current-value"]}>{value}</output>"
+      </p>
+      {error && <div className={styles.error}>{error}</div>}
+      <div className={styles["buttons-container"]}>
+        <button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
+        <button className={styles.button} disabled={!isValueVaild} onClick={onAddButtonClick}>Добавить в список</button>
+      </div>
+      <div className={styles["list-container"]}>
+        <h2 className={styles["list-heading"]}>Список:</h2>
+        {!list.length && <p className={styles["no-margin-text"]}>Нет добавленных элементов</p>}
+        
+        <ul className={styles.list}>
+          {list.map(({id, value}) => <li key={id} className={styles["list-item"]}>{value}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
